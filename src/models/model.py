e50da698165c410f07217a9b169655143cda63ef
ferran <ferranb@gmail.com>
2025-05-19 16:16:27 +0200

    Modified some parameters to slightly improve the results.

--- src/models/model.py
+++ src/models/model.py
@@ -4,16 +4,19 @@ import torch.nn.functional as f
 
 class BasicTaxoModel(nn.Module):
     def __init__(self,
-                 input_size: int, # kmer size
+                 input_size: int,
                  hidden_size: int,
-                 output_size: int = 128, # num_classes
+                 output_size: int,
                  ):
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size//2)
+        self.fc3 = nn.Linear(hidden_size//2, output_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.fc1(x)
         x = f.relu(x)
         x = self.fc2(x)
+        x = f.relu(x)
+        x = self.fc3(x)
         return x
