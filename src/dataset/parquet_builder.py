774df285a318c43355bbb9ae6bd0c30d847dffd1
ferran <ferranb@gmail.com>
2025-05-23 09:43:01 +0200

    Added support for 4 row encoding and some code rewrite.

--- src/dataset/parquet_builder.py
+++ src/dataset/parquet_builder.py
@@ -1,12 +1,11 @@
 import os
 import pandas as pd
-from dataset.utils import get_default_data_dir
-from utils import info, get_parquet_path, encoding_column_name
+from dataset.utils import get_default_data_dir, info, get_parquet_path, encoding_column_name
 from feature_extraction.main import SequenceCoder
 
 class ParquetBuilder:
     CATEGORY_COLUMNS = ["kingdom_name", "phylum_name", "class_name", "order_name"]
-    KMERS_SIZES = range(1, 6+1)
+    KMERS_SIZES = range(1, 5+1)
 
     def __init__(self,
                  csv_path: str = get_default_data_dir()+"/dataset.csv",
@@ -34,26 +33,20 @@ class ParquetBuilder:
         info(f"Loaded {len(df)} rows")
         return df
 
-    def create_parquets(self, skip_if_exists: bool = True):
+    def create_dataset_parquet(self, skip_if_exists: bool = True):
         if self.df is None:
-            raise RuntimeError("You must load before saving")
+            raise RuntimeError("You must load the database first")
 
-        self.df.to_parquet(path=get_parquet_path(self.csv_path), compression="gzip")
+        path = get_parquet_path(self.csv_path)
+        if not os.path.exists(path) or not skip_if_exists:
+            self.df.to_parquet(path, compression="gzip")
+        else:
+            info(f"Skipping main because exists {path}")
 
-        for bits in self.sequence_coder.bit_mapping:
-            col_name = encoding_column_name(bits=bits)
-            path = get_parquet_path(self.csv_path, bits=bits)
-            if os.path.exists(path) and skip_if_exists:
-                info(f"Skipping {bits=} because exists {path}")
-                continue
-            info(f"Encoding {col_name}")
-            results = [self.sequence_coder.dna_to_bitcoding_optimized(s, bits)
-                       for s in self.df[self.sequence_column_name]]
-            info(f"Moving results to a dataframe")
-            df = pd.DataFrame({col_name: results})
-            info(f"Saving '{path}'")
-            df.to_parquet(path=path, compression="gzip")
-            del df
+
+    def create_kmer_parquets(self, skip_if_exists: bool = True):
+        if self.df is None:
+            raise RuntimeError("You must load the database first")
 
         for k in ParquetBuilder.KMERS_SIZES:
             col_name = encoding_column_name(k=k)
@@ -61,25 +54,106 @@ class ParquetBuilder:
             if os.path.exists(path) and skip_if_exists:
                 info(f"Skipping {k=} because exists {path}")
                 continue
+
             info(f"Encoding {col_name}")
-            results = [self.sequence_coder.kmerize_one_seq_optimized(s, k)
+            assert_s = self.df[self.sequence_column_name][0]
+            assert len(self.sequence_coder.coding_kmer_optimized([assert_s], k)) == 1
+            results = [self.sequence_coder.coding_kmer_optimized([s], k)
                        for s in self.df[self.sequence_column_name]]
+            assert all(len(r)==1 for r in results)
+            results = [r[0] for r in results]
+
             info(f"Moving results to a dataframe")
             df = pd.DataFrame({col_name: results})
+
             info(f"Saving '{path}'")
-            df.to_parquet(path=path, compression="gzip" if k <= 6 else None)
+            df.to_parquet(path=path, compression="gzip")
             del df
 
+    def create_bit_parquets(self, skip_if_exists: bool = True):
+        if self.df is None:
+            raise RuntimeError("You must load the database first")
+
+        for bits in self.sequence_coder.bit_mapping:
+            col_name = encoding_column_name(bits=bits)
+            path = get_parquet_path(self.csv_path, bits=bits)
+            if os.path.exists(path) and skip_if_exists:
+                info(f"Skipping {bits=} because exists {path}")
+                continue
+
+            info(f"Encoding {col_name}")
+            results = [self.sequence_coder.coding_one_hot_bit_optimized([s], bits)
+                       for s in self.df[self.sequence_column_name]]
+            results = [r[0] for r in results]
+
+            info(f"Moving results to a dataframe")
+            df = pd.DataFrame({col_name: results})
+
+            info(f"Saving '{path}'")
+            df.to_parquet(path=path, compression="gzip")
+            del df
+
+    def create_4row_parquet(self, skip_if_exists: bool = True):
+        if self.df is None:
+            raise RuntimeError("You must load the database first")
+
+        path = get_parquet_path(self.csv_path, bits=0)
+        if os.path.exists(path) and skip_if_exists:
+            info(f"Skipping '4 row matrix' because exists {path}")
+        else:
+            col_name = "4row"
+            info(f"Encoding {col_name}")
+            s = self.df[self.sequence_column_name][0]
+            s = self.sequence_coder.coding_one_hot_4rowMatrix_optimized([s], return_tensor=False)
+            results = [self.sequence_coder.coding_one_hot_4rowMatrix_optimized([s], return_tensor=False)
+                       for s in self.df[self.sequence_column_name]]
+
+            info(f"Moving results to a dataframe")
+            # We do this way to avoid "PerformanceWarning: DataFrame is highly fragmented."
+            assert all(len(r) == 1 for r in results)
+            assert all(r[0].shape[0] == 4 for r in results)
+            df = pd.DataFrame({
+                f"{col_name}_1": [r[0][0] for r in results],
+                f"{col_name}_2": [r[0][1] for r in results],
+                f"{col_name}_3": [r[0][2] for r in results],
+                f"{col_name}_4": [r[0][3] for r in results],
+            })
+
+            info(f"Saving '{path}'")
+            df.to_parquet(path=path, compression="gzip")
+            del df
+
+
+    def create_parquets(self, skip_if_exists: bool = True):
+        """
+        encoder.coding_kmer_optimized(sequences=["ACGTT"], k=3) k 1..5
+        encoder.coding_one_hot_4rowMatrix_optimized(sequences=["ACGTT"], return_tensor=True)
+        encoder.coding_one_hot_bit_optimized(sequences=["ACGTT"], bits=4, return_tensor=True) 1..4
+        """
+
+        self.create_dataset_parquet(skip_if_exists=skip_if_exists)
+        self.create_kmer_parquets(skip_if_exists=skip_if_exists)
+        self.create_bit_parquets(skip_if_exists=skip_if_exists)
+        self.create_4row_parquet(skip_if_exists=skip_if_exists)
         info(f"Parquets created")
 
-    def info_parquets(self):
+
+    def show_info_parquets(self):
         def print_basic_info(path):
             info(os.path.basename(path))
             df = pd.read_parquet(path=path)
             info(f"   {df.memory_usage(deep=True).sum()/1024/1024:.2f} MB")
             info(f"   {len(df)} rows")
             info(f"   {[c for c in df.columns]}")
-            info(str(df.head()))
+            if len(df.columns) > 1:
+                info(str(df.head()))
+            else:
+                for c in df.columns:
+                    info(f"   {c}")
+                    info(f"      Min length: {df[c].apply(len).min()}")
+                    info(f"      Mean length: {df[c].apply(len).mean()}")
+                    info(f"      Max length: {df[c].apply(len).max()}")
+                    info(f"      First row: {df[c]}")
             del df
 
         p = get_parquet_path(self.csv_path)
@@ -96,7 +170,7 @@ class ParquetBuilder:
 def __main():
     p = ParquetBuilder()
     p.create_parquets() # Uncomment if any parquet file does not exists
-    p.info_parquets()
+    p.show_info_parquets()
 
 if __name__ == "__main__":
     __main()
