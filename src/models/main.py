0ba0c8360c92ae4a4df62b9e306fe2bb5df7a049
ferran <ferranb@gmail.com>
2025-05-23 11:23:12 +0200

    Model adapted to the latest changes

--- src/models/main.py
+++ src/models/main.py
@@ -2,49 +2,19 @@ import time
 import torch
 import torch.nn as nn
 import numpy as np
+import json
 from results import Results, plot_results
-from dataset.taxo_dataloader import TaxoDataLoaders
+from dataset.taxo_dataloaders import TaxoDataLoaders
 from model import BasicTaxoModel
-from dataset.utils import info
-from feature_extraction.main import kmer_encoder
+from dataset.utils import info, get_default_dataset_path
 
-HPARAMS = dict(
-    taxo_path='/tmp/database.csv',
-    max_rows=100000,
-    seed=123,
-    # kmer_k=2
-    epochs=20,
-    batch_size=32,
-    learning_rate=0.001,
-    sequence_encoder=kmer_encoder,
-    # input_size= Assigned depending on the dataset,
-    hidden_size=None,
-    # output_size= Assigned depending on the dataset
-)
-
-
-def init_device() -> torch.device:
-    np.random.seed(HPARAMS['seed'])
-    torch.manual_seed(HPARAMS['seed'])
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-        torch.cuda.manual_seed(HPARAMS['seed'])
-        return torch.device("cuda")
-    elif torch.xpu.is_available():
-        torch.xpu.empty_cache()
-        torch.xpu.manual_seed(HPARAMS['seed'])
-        return  torch.device("xpu")
-    else:
-        return  torch.device("cpu")
-
-device: torch.device = init_device()
-info(f"Using torch device: {device}")
 
 def train_epoch(results: Results,
                 network: nn.Module,
                 data_loader: torch.utils.data.DataLoader,
                 criterion: torch.nn.functional,
-                optimizer: torch.optim.Optimizer
+                optimizer: torch.optim.Optimizer,
+                device: torch.device
                 ):
     network.train()
     results.new_epoch()
@@ -62,7 +32,8 @@ def train_epoch(results: Results,
 def eval_epoch(results: Results,
                network: nn.Module,
                data_loader: torch.utils.data.DataLoader,
-               criterion: torch.nn.functional):
+               criterion: torch.nn.functional,
+               device: torch.device):
     network.eval()
     results.new_epoch()
     for data, target in data_loader:
@@ -72,14 +43,17 @@ def eval_epoch(results: Results,
         results.append(loss.item(), output, target)
 
 
-def train(hparams: dict) -> (Results, Results, Results):
-    taxo_data_loaders = TaxoDataLoaders(taxo_path=hparams["taxo_path"],
-                                        sequence_encoder=hparams["sequence_encoder"],
+def train(hparams: dict, device: torch.device) -> (Results, Results, Results):
+    taxo_path = hparams['taxo_path'] if hparams['taxo_path'] else get_default_dataset_path()
+    taxo_data_loaders = TaxoDataLoaders(taxo_path=taxo_path,
+                                        label_column_name=hparams["label_column_name"],
+                                        k=hparams["k"],
+                                        bits=hparams["bits"],
                                         batch_size=hparams["batch_size"],
                                         max_rows=hparams["max_rows"])
 
     network = BasicTaxoModel(
-        input_size=taxo_data_loaders.data_length,
+        input_size=taxo_data_loaders.data_length, # 4**k
         hidden_size=hparams['hidden_size'] if hparams['hidden_size'] else taxo_data_loaders.data_length // 2, # TODO:
         output_size=taxo_data_loaders.num_labels,
     ).to(device)
@@ -95,11 +69,11 @@ def train(hparams: dict) -> (Results, Results, Results):
     for epoch in range(hparams['epochs']):
         info(f"--- Epoch {epoch} ---")
         train_epoch(results=train_results, network=network, data_loader=taxo_data_loaders.train_loader,
-                    criterion=criterion, optimizer=optimizer)
+                    criterion=criterion, optimizer=optimizer, device=device)
         eval_epoch(results=eval_results, network=network, data_loader=taxo_data_loaders.eval_loader,
-                   criterion=criterion)
+                   criterion=criterion, device=device)
         eval_epoch(results=test_results, network=network, data_loader=taxo_data_loaders.train_loader,
-                   criterion=criterion)
+                   criterion=criterion, device=device)
         # scheduler.step(eval_results.losses[-1])
     train_results.finish()
     eval_results.finish()
@@ -108,18 +82,38 @@ def train(hparams: dict) -> (Results, Results, Results):
 
     return train_results, eval_results, test_results
 
+def init_device(hparams: dict) -> torch.device:
+    np.random.seed(hparams['seed'])
+    torch.manual_seed(hparams['seed'])
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.manual_seed(hparams['seed'])
+        d = torch.device("cuda")
+    elif torch.xpu.is_available():
+        torch.xpu.empty_cache()
+        torch.xpu.manual_seed(hparams['seed'])
+        d = torch.device("xpu")
+    else:
+        d = torch.device("cpu")
+    info(f"Device: {d}")
+    return d
 
 def run_model(hparams: dict):
-    all_results = train(hparams)
+    device = init_device(hparams)
+    all_results = train(hparams, device)
     for r in all_results:
         info(str(r))
     plot_results(all_results)
 
 
 def main():
+    with open("hparams.json", "r") as f:
+        hparams = json.load(f)
+    if hparams['taxo_path'] == "":
+        hparams['taxo_path'] = get_default_dataset_path()
     t0 = time.time()
     info("Starting")
-    run_model(HPARAMS)
+    run_model(hparams)
     info("Done")
     seconds = time.time() - t0
     minutes = int(seconds / 60)
