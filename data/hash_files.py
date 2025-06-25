c7d2eb766256df31d96479a2487108b32ed11e9e
ferran <ferranb@gmail.com>
2025-06-25 23:36:31 +0200

    Modified hash_files.py to the new data folders

--- data/hash_files.py
+++ data/hash_files.py
@@ -1,25 +1,24 @@
 import hashlib
 import os
-from dataset.utils import get_default_data_dir
+from pathlib import Path
+from dataset.utils import get_base_parquets_path
 
-def parquet_hashes(path: str) -> list[list[str]]:
+def parquet_hashes(path: Path) -> list[list[str]]:
     results = list()
 
-    file_names = [f'dataset_kmer_{k}.parquet' for k in range(1, 4+1)]
-    file_names += [f'dataset_bits_{b}.parquet' for b in range(1, 4+1)]
-    file_names.append('dataset_4rowmatrix.parquet')
-
-    for file_name in file_names:
+    for file_path in path.iterdir():
         sha256 = hashlib.sha256()
-        file_path = os.path.join(path, file_name)
         with open(file_path, "rb") as f:
             for piece in iter(lambda: f.read(4096), b""):
                 sha256.update(piece)
         h = sha256.hexdigest()
-        results.append([file_name, h])
+        results.append([path.name + os.sep + file_path.name, h])
 
     return results
 
 
 if __name__ == "__main__":
-    [print(file_name, hash_) for file_name, hash_ in parquet_hashes(get_default_data_dir())]
+    for d in get_base_parquets_path().iterdir():
+        if not d.is_dir():
+            continue
+        [print(file_name, hash_) for file_name, hash_ in parquet_hashes(d)]
